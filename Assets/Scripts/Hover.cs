9a9cb6cad51bc216d4627160359e855f5a857e7b
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2024-02-24 13:21:14 +0100

    wip: dunno

---
+++ Assets/Scripts/Hover.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Hover : MonoBehaviour
+{
+    public void HoverOver()
+    {
+        Console.WriteLine("yo");
+    }
+}
