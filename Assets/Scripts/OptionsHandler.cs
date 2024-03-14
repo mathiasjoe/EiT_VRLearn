a7d5450c0840820b4b1fd68660dfb3e69a25fc59
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2024-03-14 18:11:57 +0100

    feat: reset button

--- Assets/Scripts/OptionsHandler.cs
+++ Assets/Scripts/OptionsHandler.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class OptionsHandler : MonoBehaviour
 {
 
     private GameObject[] organs;
+    private GameObject[] goals;
+
+    private Dictionary<GameObject, string> goalsBaseText = new Dictionary<GameObject, string>();
 
     private Dictionary<GameObject, Vector3> originalPositions = new Dictionary<GameObject, Vector3>();
     private Dictionary<GameObject, Quaternion> originalRotations = new Dictionary<GameObject, Quaternion>();
@@ -13,7 +17,6 @@ public class OptionsHandler : MonoBehaviour
     void Start()
     {
         organs = GameObject.FindGameObjectsWithTag("Organ");
-
         foreach (var organ in organs)
         {
             if (organ != null)
@@ -22,16 +25,39 @@ public class OptionsHandler : MonoBehaviour
                 originalRotations[organ] = organ.transform.rotation;
             }
         }
+
+        goals = GameObject.FindGameObjectsWithTag("Goal");
+        foreach (var goal in goals)
+        {
+            string text = goal.GetComponent<TextMeshProUGUI>().text;
+            goalsBaseText[goal] = text;
+        }
     }
 
-    public void ResetOrganPosition()
+    public void ResetGame()
     {
+        // Reset position of organs
         foreach (var organ in organs)
         {
             if (organ != null && originalPositions.ContainsKey(organ) && originalRotations.ContainsKey(organ)) 
             { 
+                Rigidbody rb = organ.GetComponent<Rigidbody>();
+                rb.isKinematic = false;
+                rb.velocity = Vector3.zero;
+
                 organ.transform.position = originalPositions[organ];
                 organ.transform.rotation = originalRotations[organ];
+            }
+        }
+
+        // Reset checklist
+        foreach (var goal in goals)
+        {
+            TextMeshProUGUI textComponent = goal.GetComponent<TextMeshProUGUI>();
+            if (textComponent != null && goalsBaseText.ContainsKey(goal))
+            {
+                textComponent.color = Color.white;
+                textComponent.text = $"{goalsBaseText[goal]}";
             }
         }
     }
